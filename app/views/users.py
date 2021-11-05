8fce5696c492940ee3918a0d5d4b8f195cb9a0f1
leonardo <leonardor.toledo@gmail.com>
2021-11-05 19:07:20 -0300

    API initial

---
+++ app/views/users.py
@@ -0,0 +1,82 @@
+from werkzeug.security import generate_password_hash
+from app import db
+from flask import json, jsonify, request
+from ..models.users import Users, user_schema, users_schema
+
+def post_user():
+    username = request.json['username']
+    password = request.json['password']
+    name = request.json['name']
+    email = request.json['email']
+    pass_hash = generate_password_hash(password)
+    user = Users(username, pass_hash, name, email)
+
+    try:
+        db.session.add(user)
+        db.session.commit()
+        result = user_schema.dump(user)
+        return jsonify({'mensagem': 'registrado com sucesso', 'data': result}), 201
+    except Exception as e:
+        print(e)
+        return jsonify({'mensagem': 'erro ao criar', 'data': {}}), 500
+
+def update_user(id):
+    username = request.json['username']
+    password = request.json['password']
+    name = request.json['name']
+    email = request.json['email']
+    
+    user = Users.query.get(id)
+
+    if not user:
+        return jsonify({'mensagem': 'o usuario nao existe', 'data': {}}), 404
+
+    pass_hash = generate_password_hash(password)
+
+    try:
+        user.username = username
+        user.password = pass_hash
+        user.name = name
+        user.email = email
+        db.session.commit()
+        result = user_schema.dump(user)
+        return jsonify({'mensagem': 'atualizado com sucesso', 'data': result}), 201
+    except Exception as e:
+        print(e)
+        return jsonify({'mensagem': 'erro ao atualizar', 'data': {}}), 500
+
+def get_users():
+    users = Users.query.all()
+    if users:
+        result = users_schema.dump(users)
+        return jsonify({'mensagem': 'usuarios carregados', 'data': result})
+
+    return jsonify({'mensagem': 'nada encontrado', 'data': {}})
+
+def get_user(id):
+    user = Users.query.get(id)
+    if user:
+        result = user_schema.dump(user)
+        return jsonify({'mensagem': 'usuario carregado', 'data': result}), 210
+
+    return jsonify({'mensagem': 'nada encontrado', 'data': {}}), 404
+
+def delete_user(id):
+    user = Users.query.get(id)
+    if not user:
+        return jsonify({'mensagem': 'usuario nao encontrado', 'data': {}}), 404
+
+    if user:
+        try:
+            db.session.delete(user)
+            db.session.commit()
+            result = user_schema.dump(user)
+            return jsonify({'mensagem': 'usuario deletado', 'data': result}), 200
+        except:
+            return jsonify({'mensagem': 'erro ao deletar', 'data': {}}), 500
+
+def user_by_username(username):
+    try:
+        return Users.query.filter(Users.username == username).one()
+    except:
+        return None
